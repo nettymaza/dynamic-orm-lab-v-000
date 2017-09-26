1abf4839b6af7c3485836847544035d1daae6ffe
Annette Maza <nettymaza@gmail.com>
2017-09-26 04:09:12 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -59,9 +59,9 @@ class InteractiveRecord
     value_for_insert = attribute_value.class == Fixnum ? attribute_value : "'#{attribute_value}'"
     sql = "SELECT * FROM #{self.table_name} WHERE #{attribute.keys.first} = #{value_for_insert}"
     DB[:conn].execute(sql)
+    binding.pry
 
   end
-
 
 
   # def self.find_by(attribute_hash)
