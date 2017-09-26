2adb36b872ab68ae9a0b24f3a56e585a5a029f2b
Annette Maza <nettymaza@gmail.com>
2017-09-26 04:23:55 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -56,6 +56,7 @@ class InteractiveRecord
 
   def self.find_by(attribute)
     attribute_value = attribute.values.first
+    #if the value_for_insert == a number then return number else return attribute_value
     value_for_insert = attribute_value.class == Fixnum ? attribute_value : "'#{attribute_value}'"
     sql = "SELECT * FROM #{self.table_name} WHERE #{attribute.keys.first} = #{value_for_insert}"
     DB[:conn].execute(sql)
